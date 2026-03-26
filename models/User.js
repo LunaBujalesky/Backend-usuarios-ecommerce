a931e8f6eaedf9cbca756630ac186fd96f3c6518
Luna Bujalesky <lunitabruja@gmail.com>
2026-03-26 01:12:23 -0300

    correccion

--- models/User.js
+++ models/User.js
@@ -6,14 +6,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, unique: true },
     age: Number,
     password: String,
-    cart: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "carts"
-    },
-    role: {
-        type: String,
-        default: "user"
-    }
+    role: { type: String, default: "user" }
 });
 
 export default mongoose.model("users", userSchema);
